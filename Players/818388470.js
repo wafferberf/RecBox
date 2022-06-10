f0c2d7775110b417492a4a208918b142c9d5d14c
SlideVRalt <106561594+SlideVRalt@users.noreply.github.com>
2022-06-10 18:35:13 -0400

    Commandcookie's level changed to 23 (by request)

--- Players/818388470.js
+++ Players/818388470.js
@@ -1,1 +1,1 @@
-{"Id":818388470,"DisplayName":"Commandcookie","Username":"Commandcookie","Level":30,"XP":100,"Developer":false,"IsBooster":false,"BootMultiplier":0}
+{"Id":818388470,"DisplayName":"Commandcookie","Username":"Commandcookie","Level":23,"XP":100,"Developer":false,"IsBooster":false,"BootMultiplier":0}
